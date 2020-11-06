9a11293516536f0cb0e0162011c393645ef10acd
Sugandha Singhania <sugandha.singhania36@gmail.com>
2020-11-06 16:16:00 +0530

    changes in create.py

--- create.py
+++ create.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
 import mysql.connector
 
 mydb = mysql.connector.connect(
@@ -14,12 +14,31 @@ mycursor = mydb.cursor()
 
 app = Flask(__name__)
 
+@app.route('/dummy',methods=['POST', 'GET'])
+def dummy():
+    print("sfkjsjgndjl")
+    return "hi"
+
+
 @app.route('/')
 def index():
-    return "Hello, World!"
+  # mycursor.execute("SELECT * FROM interview")
+
+  # myresult = mycursor.fetchall()
+  return render_template("form.html")
+
 
 @app.route('/new_interview',methods=['POST', 'GET'])
-def new_interview(interviewer, interviewee, start, end, date):
+def new_interview():
+
+    json_data = request.json
+    interviewer_name = json_data["interviewer_name"]
+    interviewee_name = json_data["interviewee_name"]
+    interviewer_email = json_data["interviewer_email"]
+    interviewee_email = json_data["interviewee_email"]
+    date = json_data["interview_date"]
+    start= json_data["start_time"]
+    end = json_data["end_time"]
 
 
     mycursor.execute("SELECT * FROM interview")
@@ -28,57 +47,64 @@ def new_interview(interviewer, interviewee, start, end, date):
 
     flag=0
     for x in myresult:
-      if ((interviewer==x.interviewer_id or interviewee==x.interviewee_id) and date==x.interview_date and ((start>=x.start_time and start<=x.end_time ) or (end>=x.start_time and end<=x.end_time ))):
+      if ((interviewer_email==x[6] or interviewee_email==x[7]) and date==x[3] and ((start>=x[4] and start<=x[5] ) or (end>=x[4] and end<=x[5] ))):
           flag=1
           print(x)
 
     if flag==1:
         return "error"
     else:
-        mycursor.execute("""INSERT INTO interview (name,start_time,end_time) VALUES (?,?,?)""",(name,start,end))
+        mycursor.execute("""INSERT INTO interview (interviewer_name, interviewee_name, interviewer_email, interviewee_email ,interview_date, start_time,end_time) VALUES (%s, %s, %s, %s, %s, %s, %s)""",(interviewer_name, interviewee_name, interviewer_email, interviewee_email , date, start, end ))
         mydb.commit()
         return "record inserted."
 
 @app.route('/edit_interview',methods=['POST', 'GET'])
-def edit_interview(interview_id):
-    mycursor.execute("SELECT * FROM interview")
-
+def edit_interview():
+    json_data = request.json
+    interview_id = json_data["interview_id"]
+    sql = "SELECT *  FROM interview WHERE id ='%d'" % (interview_id)
+    mycursor.execute(sql)
     myresult = mycursor.fetchall()
     for x in myresult:
-        return x
-
-
+        return jsonify(x)
 
 @app.route('/submit_edit_interview',methods=['POST', 'GET'])
-def submit_edit_interview(interview_id,):
+def submit_edit_interview():
+    json_data = request.json
+    interviewer_name = json_data["interviewer_name"]
+    interviewee_name = json_data["interviewee_name"]
+    interviewer_email = json_data["interviewer_email"]
+    interviewee_email = json_data["interviewee_email"]
+    date = json_data["interview_date"]
+    start= json_data["start_time"]
+    end = json_data["end_time"]
+    interview_id=json_data["interview_id"]
 
 
     mycursor.execute("SELECT * FROM interview")
-
     myresult = mycursor.fetchall()
 
     flag=0
     for x in myresult:
-      if ((interviewer==x.interviewer_id or interviewee==x.interviewee_id) and date==x.interview_date and ((start>=x.start_time and start<=x.end_time ) or (end>=x.start_time and end<=x.end_time ))):
+      if ((interviewer_email==x[6] or interviewee_email==x[7]) and date==x[3] and ((start>=x[4] and start<=x[5] ) or (end>=x[4] and end<=x[5] ))):
           flag=1
           print(x)
 
     if flag==1:
         return "error"
     else:
-        mycursor.execute("""INSERT INTO interview (name,start_time,end_time) VALUES (?,?,?)""",(name,start,end))
+        mycursor.execute("UPDATE interview set interviewer_name = %s, interviewee_name = %s, interviewer_email = %s, interviewee_email = %s ,interview_date = %s, start_time = %s, end_time = %s where id= %s", (interviewer_name, interviewee_name, interviewer_email, interviewee_email , date, start, end, interview_id));
         mydb.commit()
         return "record inserted."
 
 @app.route('/delete_interview',methods=['POST', 'GET'])
-def delete_interview(interview_id):
-    sql = "DELETE FROM interview WHERE id == '%d'" % (interview_id)
-
-    try:
-       cursor.execute(sql)
-       mydb.commit()
-    except:
-       mydb.rollback()
+def delete_interview():
+    json_data = request.json
+    interview_id = json_data["interview_id"]
+    sql = "DELETE FROM interview WHERE id ='%d'" % (interview_id)
+    mycursor.execute(sql)
+    mydb.commit()
+    return "deleted"
 
 
 if __name__ == '__main__':
